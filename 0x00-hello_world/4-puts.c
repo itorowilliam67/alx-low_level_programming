713ab17d9320612b45cf3169ff381ecde83753f5
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-08 19:10:40 -0700

    changes to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+
+
+
 /**
- * main - Printing mulitlingual puzzle
  *
- * Return always 0 (succeful output)
- */
+ * * main - Printing
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
 }
