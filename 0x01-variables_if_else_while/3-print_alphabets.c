89850ae28c96adc291b2cd61db1f62a1bde0f999
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-11 20:47:27 -0700

    lower and upper case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/** 
+ * this section of the code will print alphabet in lowercase
+ * followed by a new line
+ */
+
+
+int main() {
+
+	  char b = 'a';
+
+	    
+
+	    while (b <= 'z') {
+
+		        printf("%c\n", b);
+
+			    b++;
+
+			      }
+/** The section will print alphabet in uppercase
+ *
+ *followed by a new line
+ */
+	      
+
+	      char ba = 'A';
+
+	        
+
+	        while (ba <= 'Z') {
+
+			    printf("%c\n", ba);
+
+			        ba++;
+
+				  }
+
+		  
+
+		  return 0;
+
+}
