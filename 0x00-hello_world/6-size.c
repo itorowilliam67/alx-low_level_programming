f8f22506afbd2b833672d5d29a4ce4eb4e03665f
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-12 17:18:25 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
+ *main - Prints the sizeof many var types
  *
- * * main - Prints the sizeof many var types
+ *Return: Always 0 (Success)
  *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+ */
 
 int main(void)
 
