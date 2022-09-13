de2a27aaaa09342d2c9cfb880119e611d99a1bbc
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-12 20:27:45 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  * this program will print the alphabet followed by a new line
- * th '\n' command will break the output in to a new line
+ * the '\n' command will break the output in to a new line
  */
-int main ()
+int main (void)
 {
-char ch = 'a';
-while (ch <= 'z')
-{
-	printf("%c\n",ch);
-	ch++;
-}
+	char c;
+	c = 'a';
+	while (c <= 'z') {
+		putchar (c);
+		c++;
+	}
+	putchar("\n");
 return 0;
 }
