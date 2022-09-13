ce295a9e9bad258a3005f21a9f4a8f0e0710389e
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-12 17:50:10 -0700

    different numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,22 @@
+i#include <stdlib.h>
+
+#include <time.h>
+
+/** more headers goes there
+ * always return 0
+ * main entry point 
+* betty style doc for function main goes there
+*/
+
+int main(void)
+
+{
+		int n;
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**main-entry point
- *
- * lways return successful
- */
+ 
 int main(void){
 	int n;
 	srand(time(0));
@@ -15,6 +27,17 @@ int main(void){
 		printf("%i is Zero\n",n);}
 	else{ 
 		printf("%i is positive\n",n);}
+		
 	return 0;
 }
 
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+				
+
+				return (0);
+
+}
