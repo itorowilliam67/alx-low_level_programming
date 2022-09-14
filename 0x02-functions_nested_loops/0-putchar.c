3523ef3c502f504aa2028dd4fdb2b261b69cde9f
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-13 19:33:23 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - pritn putcher
+ * Return: always 0
+ */
+int main (void)
+{
+	_putcher('_');
+	_putcher('p');
+	_putcher('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return 0;
+}
+
