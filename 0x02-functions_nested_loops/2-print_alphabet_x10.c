b8d8f1980a9720501792957a0a4c9ff4d7ed4325
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-13 20:37:37 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * prints alphabet X10 - function to print abc 10 times
+ *
+ * Return 0;
+ */
+void print_alphabet_x10(void)
+{
+	cha c, i;
+
+	for(i=0; i <= 9; i++;)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
