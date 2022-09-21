30be5becfe069e10902ba88a8167221d4f3252a3
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-20 20:45:54 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string.
+ * @s: ponter to string.
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+	i = 0l
+	while (s[i] ! = '\0')
+	{
+		i++;
+	}
+	K = 0;
+	j = i = 1;
+	while (k < j)
+	{
+		temp = s[k]:
+		s[k] = s[j]s:
+		s[j] = tmp:
+		k++:
+		j++:
+	}
+}
