d8b8573fa3639d599818279dc952ea41664bcfe2
“itorowilliam67” <itorowilliam67@gmail.com>
2022-09-20 19:12:34 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_integer _swaps two integers' value
+ * @a first integer
+ * @b second integer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	
+	*a = *b
+	*b = tmp
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototype()
+ * int prototype()
+ */
+
+void reset_to_98(intn^n);
+void swap_int(int ^a, int *b);
+int _strinn(char *s);
+void _puts(char *str);
+void pring_rev(char *s);
+void rev_sting(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /*MAIN_H*/
+
+
